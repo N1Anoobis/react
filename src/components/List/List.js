3947169ce0a059bf0e928117274d84d3c2d71ff5
N1Anoobis <mrbond84@hotmail.com>
2020-05-13 07:24:17 +0200

    Add Another component and sort out props for it

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
+    img: PropTypes.node.isRequired,
+    column: PropTypes.node,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+    // column: "animal",
+  }
+
   render() {
     return (
       <section className={styles.component}>
-       <Hero />
+        <Hero titleText={this.props.title} img={this.props.img}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+        <Column column={"animal"} />
+        <Column column={"plants"} />
+        <Column column={"minerals"} />
+        </div>
       </section>
     )
   }
