adbd8bbe2895ed36ade8d67cb4ddde4359f382e5
N1Anoobis <mrbond84@hotmail.com>
2020-05-14 17:26:01 +0200

    Add component Card and Icon

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+const Card = (props) => (
+   
+ <section  className={styles.component}>
+     <h3 className={styles.title}>{props.title}</h3>
+ </section>
+);
+
+Card.propTypes = {
+  column: PropTypes.node,
+};
+
+export default Card;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,86 @@
 import React from 'react';
+import Icon from '../Icon/Icon';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
 
-const Column = (props) => (
- <section className={styles.component}>
-     <h3 className={styles.title}>{props.column}</h3>
- </section>
-);
+import Creator from '../Creator/Creator';
+import { settings } from '../../data/dataStore';
 
-Column.propTypes = {
-//   titleText: PropTypes.node,
-  column: PropTypes.node,
-};
+class Column extends React.Component {
 
+  state = {
+    cards: this.props.cards || [],
+  }
+
+
+  // static propTypes = {
+  //     title: PropTypes.node.isRequired,
+  // children: PropTypes.node.isRequired,
+  // image: PropTypes.node.isRequired,
+  //   column: PropTypes.node,
+  //   description: PropTypes.node,
+  //   columns: PropTypes.array,
+  // }
+
+  // static defaultProps = {
+  // children: <p>I can do all the things!!!</p>,
+  // description: settings.defaultListDescription,
+  // column: "animal",
+  // }
+
+
+  addCard(title) {
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
+
+
+
+
+
+
+
+  render() {
+    return (
+      < section className={styles.component} >
+        <h3 className={styles.title}>{this.props.title}
+          <span className={styles.icon}>
+            {/* {this.state.cards.map(({ key }) => ( */}
+              <Icon key='3' name={this.props.icon} />
+
+            {/* // ))} */}
+          </span>
+        </h3>
+
+        {/* {...console.log(this.props.icon)} */}
+
+        <div className={styles.cards}>
+          {this.state.cards.map(({ key, ...columnProps }) => (
+            <Card key={key} {...columnProps} />
+
+          ))}
+
+        </div>
+
+
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        </div>
+      </section >
+    )
+  }
+
+}
 export default Column;
